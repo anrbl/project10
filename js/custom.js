7437c1c787f4dfac880f48a5534bfd09426f7944
anrbl <133613567+anrbl@users.noreply.github.com>
2023-07-18 18:16:34 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,28 @@
 $(function () {
     $(window).on('scroll', function () {
         let sct = $(window).scrollTop();
+        sct > 100 ? $('.to_top').fadeIn() : $('.to_top').fadeOut(0)
         sct > 0 ? $('.header').addClass('on') : $('.header').removeClass('on');
     });
+
+    $('.to_top').on('click', function () {
+        $('html,body').animate({ scrollTop: 0 }, 600);
+    });
+
+    //mainslide
+
+    $('.main_slide').on('init afterChange', function (e) {
+        const current = $('.main_slide .slick-current');
+        current.addClass('on').siblings().removeClass('on');
+    })
     $('.main_slide').slick({
         arrows: false,
         fade: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 800,
+        pauseOnHover: false,
+        pauseOnFocus: false,
     });
 
     $('.premium_line').slick({
