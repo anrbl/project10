65486726b642b50e3421340d48601bfc927e6525
anrbl <133613567+anrbl@users.noreply.github.com>
2023-07-19 18:05:39 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -29,6 +29,12 @@ $(function () {
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 1,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+            }
+        }],
     });
     $('.main_premium .premium_arrows .left').on('click', function () {
         $('.premium_line').slick('slickPrev')
@@ -41,6 +47,13 @@ $(function () {
         arrows: false,
         slidesToShow: 3,
         slidesToScroll: 1,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                fade: true,
+            }
+        }],
     });
     $('.main_unit .unit_arrows .left').on('click', function () {
         $('.product_slide').slick('slickPrev')
@@ -48,5 +61,24 @@ $(function () {
     $('.main_unit .unit_arrows .right').on('click', function () {
         $('.product_slide').slick('slickNext')
     });
+
+
+    //mobile
+    $('.mmenu').on('click', function () {
+        $(this).toggleClass('on');
+        $('.gnb').toggleClass('on');
+        $('h1').toggleClass('on');
+        $('.user_area').toggleClass('on');
+    });
+    $(window).on('resize', function () {
+        $('.gnb').removeClass('on');
+    });
+    $('.gnb').on('wheel', function (e) {
+        if ($(this).hasClass('on')) {
+            e.preventDefault();
+        }
+    });
+
+
 
 });
